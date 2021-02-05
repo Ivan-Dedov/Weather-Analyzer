bad001cc2c252052ce00d6d5ddbb8df8b8374ffb
d_ivanchik <sean.grand.2002@gmail.com>
2021-02-05 18:00:50 +0300

    Completed Enums
    Completed all the required Enums and added comments.

--- Weather Analyzer/Program.cs
+++ Weather Analyzer/Program.cs
@@ -1,31 +1,43 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace WeatherAnalyzer
 {
 
+    /// <summary>
+    /// The type of the WeatherEvent.
+    /// </summary>
     public enum WeatherEventType
     {
-
+        Snow,
+        Fog,
+        Cold,
+        Storm,
+        Rain,
+        Precipitation,
+        Hail,
     }
 
+    /// <summary>
+    /// The severity of the WeatherEvent.
+    /// </summary>
     public enum Severity
     {
-
-    }
-
-    public enum TimeZone
-    {
-
+        Light,
+        Severe,
+        Moderate,
+        Heavy,
+        UNK,
+        Other,
     }
 
     class Program
     {
 
-        // Нужно дополнить модель WeatherEvent, создать список этого типа List<>
-        // И заполнить его, читая файл с данными построчно через StreamReader
-        // Ссылка на файл https://www.kaggle.com/sobhanmoosavi/us-weather-events
+        // https://www.kaggle.com/sobhanmoosavi/us-weather-events
 
         // Написать Linq-запросы, используя синтаксис методов расширений
         // и продублировать его, используя синтаксис запросов
@@ -35,38 +47,89 @@ namespace WeatherAnalyzer
         // 1. Сколько записей за каждый из годов имеется в датасете.
         // Потом будут еще запросы
 
-        public static readonly string Path = @"..\..\..\WeatherEvents_Jan2016-Dec2020.csv";
-        public static readonly string Separator = ",";
+        /// <summary>
+        /// The path to the data file.
+        /// </summary>
+        public static readonly string Path = @"..\..\..\..\..\WeatherEvents_Jan2016-Dec2020.csv";
+        /// <summary>
+        /// The string separator used in the file.
+        /// </summary>
+        public static readonly string CommaSeparatorValue = ",";
 
+        /// <summary>
+        /// The list of WeatherEvents.
+        /// </summary>
         private static List<WeatherEvent> listOfWeatherEvents = new List<WeatherEvent>();
 
         static void Main(string[] args)
         {
+            ExtractWeatherEventsFromFile(Path);
+        }
 
-            // Reading from file.
-            using (StreamReader sr = new StreamReader(Path))
+        /// <summary>
+        /// Gets all possible unique values for the weather type of the WeatherEvent and prints them to Console.
+        /// </summary>
+        private static void WriteUniqueWeatherTypesToConsole()
+        {
+            // Comment out all the extra lines beforehand and set the
+            // type of the autoproperty to string.
+            IEnumerable<WeatherEvent> uniqueWeatherTypes =
+                listOfWeatherEvents.GroupBy(x => x.Type).Select(x => x.First());
+            foreach (WeatherEvent weatherEvent in uniqueWeatherTypes)
+            {
+                Console.WriteLine(weatherEvent.Type.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Gets all possible unique values for the severity of the WeatherEvent and prints them to Console.
+        /// </summary>
+        private static void WriteUniqueWeatherSeveritiesToConsole()
+        {
+            // Comment out all the extra lines beforehand and set the
+            // type of the autoproperty to string.
+            IEnumerable<WeatherEvent> uniqueWeatherSeverities =
+                listOfWeatherEvents.GroupBy(x => x.Severity).Select(x => x.First());
+            foreach (WeatherEvent weatherEvent in uniqueWeatherSeverities)
+            {
+                Console.WriteLine(weatherEvent.Severity.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Reads the file and converts the data to a list.
+        /// </summary>
+        private static void ExtractWeatherEventsFromFile(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
             {
                 sr.ReadLine();
                 while (!sr.EndOfStream)
                 {
-                    string[] line = sr.ReadLine().Split(Separator);
-                    for (int i = 0; i < line.Length; i++)
+                    try
                     {
-                        // READ FROM FILE, PARSE AND PUT ITEMS TO THE LIST.
+                        string[] line = sr.ReadLine().Split(CommaSeparatorValue);
+                        listOfWeatherEvents.Add(new WeatherEvent()
+                        {
+                            EventID = line[0],
+                            Type = Enum.Parse<WeatherEventType>(line[1]),
+                            Severity = Enum.Parse<Severity>(line[2]),
+                            StartTime = DateTime.Parse(line[3]),
+                            EndTime = DateTime.Parse(line[4]),
+                            TimeZone = line[5],
+                            AirportCode = line[6],
+                            LocationLatitude = double.Parse(line[7], new CultureInfo("en-US")),
+                            LocationLongitude = double.Parse(line[8], new CultureInfo("en-US")),
+                            City = line[9],
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
                     }
                 }
             }
-
-            /*WeatherEvent we = new WeatherEvent()
-            {
-                EventId = "W-1",
-                Type = WeatherEventType.Rain,
-                Severity = Severity.Light,
-                StartTime = DateTime.Now
-            };*/
-
-
-            // ANALYZE ITEMS USING LINQ
         }
+
     }
 }
--- Weather Analyzer/WeatherEvent.cs
+++ Weather Analyzer/WeatherEvent.cs
@@ -2,21 +2,78 @@
 
 namespace WeatherAnalyzer
 {
+
+    /// <summary>
+    /// Describes a single instance of a Weather Event.
+    /// </summary>
     public class WeatherEvent
     {
-        public string EventId { get; set; }
+
+        /// <summary>
+        /// The ID of the WeatherEvent.
+        /// </summary>
+        public string EventID { get; set; }
+        
+        /// <summary>
+        /// The type of the WeatherEvent.
+        /// </summary>
         public WeatherEventType Type { get; set; }
+
+        /// <summary>
+        /// The severity of the WeatherEvent.
+        /// </summary>
         public Severity Severity { get; set; }
+
+        /// <summary>
+        /// The start time of the WeatherEvent.
+        /// </summary>
         public DateTime StartTime { get; set; }
+
+        /// <summary>
+        /// The end time of the WeatherEvent.
+        /// </summary>
         public DateTime EndTime { get; set; }
-        public TimeZone TimeZone { get; set; }
+
+        /// <summary>
+        /// The time zone of the WeatherEvent.
+        /// </summary>
+        public string TimeZone { get; set; }
+
+        /// <summary>
+        /// The airport code of the WeatherEvent.
+        /// </summary>
         public string AirportCode { get; set; }
+
+        /// <summary>
+        /// The latitude of the location of the WeatherEvent.
+        /// </summary>
         public double LocationLatitude { get; set; }
+
+        /// <summary>
+        /// The longitude of the location of the WeatherEvent.
+        /// </summary>
         public double LocationLongitude { get; set; }
+
+        /// <summary>
+        /// The city of the WeatherEvent.
+        /// </summary>
         public string City { get; set; }
+
+        /// <summary>
+        /// The county of the WeatherEvent.
+        /// </summary>
         public string County { get; set; }
+
+        /// <summary>
+        /// The state of the WeatherEvent.
+        /// </summary>
         public string State { get; set; }
+
+        /// <summary>
+        /// The zip code of the WeatherEvent.
+        /// </summary>
         public int ZipCode { get; set; }
 
     }
+
 }
