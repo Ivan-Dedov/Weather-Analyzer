d4eb039e2557f0f50781d2897a722755a6d5da51
d_ivanchik <sean.grand.2002@gmail.com>
2021-02-05 16:16:34 +0300

    Initial commit

---
+++ Weather Analyzer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace WeatherAnalyzer
+{
+
+    public enum WeatherEventType
+    {
+
+    }
+
+    public enum Severity
+    {
+
+    }
+
+    public enum TimeZone
+    {
+
+    }
+
+    class Program
+    {
+
+        // Нужно дополнить модель WeatherEvent, создать список этого типа List<>
+        // И заполнить его, читая файл с данными построчно через StreamReader
+        // Ссылка на файл https://www.kaggle.com/sobhanmoosavi/us-weather-events
+
+        // Написать Linq-запросы, используя синтаксис методов расширений
+        // и продублировать его, используя синтаксис запросов
+        // (возможно с вкраплениями методов расширений, ибо иногда первого может быть недостаточно)
+
+        // 0. Linq - сколько различных городов есть в датасете.
+        // 1. Сколько записей за каждый из годов имеется в датасете.
+        // Потом будут еще запросы
+
+        public static readonly string Path = @"..\..\..\WeatherEvents_Jan2016-Dec2020.csv";
+        public static readonly string Separator = ",";
+
+        private static List<WeatherEvent> listOfWeatherEvents = new List<WeatherEvent>();
+
+        static void Main(string[] args)
+        {
+
+            // Reading from file.
+            using (StreamReader sr = new StreamReader(Path))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(Separator);
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        // READ FROM FILE, PARSE AND PUT ITEMS TO THE LIST.
+                    }
+                }
+            }
+
+            /*WeatherEvent we = new WeatherEvent()
+            {
+                EventId = "W-1",
+                Type = WeatherEventType.Rain,
+                Severity = Severity.Light,
+                StartTime = DateTime.Now
+            };*/
+
+
+            // ANALYZE ITEMS USING LINQ
+        }
+    }
+}
---
+++ Weather Analyzer/WeatherEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WeatherAnalyzer
+{
+    public class WeatherEvent
+    {
+        public string EventId { get; set; }
+        public WeatherEventType Type { get; set; }
+        public Severity Severity { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeZone TimeZone { get; set; }
+        public string AirportCode { get; set; }
+        public double LocationLatitude { get; set; }
+        public double LocationLongitude { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+
+    }
+}
